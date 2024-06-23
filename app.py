328f5dbefb350c05e8b4d692d1e6f6cbeda89aa5
NigarAbdullayeva1 <8776@holbertonstudent.com>
2024-06-23 12:55:28 +0000

    app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from persistence.routes.user_routes import userRoutes
+from persistence.routes.country_routes import countryRoutes
+from persistence.routes.city_routes import cityRoutes
+from persistence.routes.amenity_routes import amenityRoutes
+from persistence.routes.place_routes import placeRoutes
+from persistence.routes.review_routes import reviewRoutes
+
+app = Flask(__name__)
+app.register_blueprint(userRoutes)
+app.register_blueprint(countryRoutes)
+app.register_blueprint(cityRoutes)
+app.register_blueprint(amenityRoutes) 
+app.register_blueprint(placeRoutes)
+app.register_blueprint(reviewRoutes)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
